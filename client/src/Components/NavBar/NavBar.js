bd31ddde83d7487e21e325512c2e4b4e357a6c92
DannyRux <dangorlov@yahoo.com>
2018-02-14 09:48:27 -0500

    Auth and state changes

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,6 +1,4 @@
 import React, {Component} from "react";
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import Auth from '../../Modules/Auth';
 import {
   Collapse,
@@ -9,31 +7,22 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+  NavLink, } from 'reactstrap';
 
-export default class NavBar extends React.Component {
+export default class NavBar extends Component {
   constructor(props) {
-     super(props);
-
-   this.toggle = this.toggle.bind(this);
+    super(props);
+    this.toggle = this.toggle.bind(this);
   }
 
   state = {
     isOpen: false,
-    isAuthenticated: false
   }
-  componentDidMount() {
-    this.setState({ isAuthenticated: Auth.isUserAuthenticated() });
-  }
-
+  
   logOut = event => {
     event.preventDefault();
     Auth.deauthenticateUser();
-    this.setState({ isAuthenticated: false });
+    this.props.userAuthChanged();
   }
 
   toggle() {
@@ -44,35 +33,34 @@ export default class NavBar extends React.Component {
 
   render() {
     return (
-      <div>
-        <Navbar color="faded" light expand="md">
-          <NavbarBrand href="/"></NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-            { this.state.isAuthenticated ? (
-              <div>
-                <NavItem>
-                  <NavLink href="/api/dashboard">User</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/logout" onClick={ this.logOut }>Logout</NavLink>
-                </NavItem>
-              </div>
-            ) : (
-              <div>
-                <NavItem>
-                  <NavLink href="/login">Log In</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/signup">Sign Up</NavLink>
-                </NavItem>
-              </div>
-            )}
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <Navbar color="faded" light expand="md">
+        <NavbarBrand href="/"></NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+          { this.props.isAuthenticated ? (
+            <div>
+              <NavItem>
+                <NavLink href="/dashboard">User</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/logout" onClick={ this.logOut }>Logout</NavLink>
+              </NavItem>
+            </div>
+          ) : (
+            <div>
+              <NavItem>
+                <NavLink 
+                  href="/login">Log In</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/signup">Sign Up</NavLink>
+              </NavItem>
+            </div>
+          )}
+          </Nav>
+        </Collapse>
+      </Navbar>
     );
   }
 }
