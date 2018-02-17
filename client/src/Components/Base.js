a929f932f4dcc37d06a079bd0d2b8783fb69efe2
Colin Shifley <ccshifley@gmail.com>
2018-02-17 16:30:02 -0500

    NavBar Aligned, Font Added, Proper Color Added

--- client/src/Components/Base.js
+++ client/src/Components/Base.js
@@ -15,13 +15,13 @@ class Base extends Component {
                     <div className="top-bar-left">
                         <Link to="/">
                             <div className='logobox'>
-                                <img src="../ImagesC/FTF-logo_OneColor_Truck.png" width="100%" padding="0.5rem, 0"></img>
+                                <img src="../ImagesC/FTF-logo_OneColor_Truck_Red.png" width="100%" padding="0.5rem, 0"></img>
                             </div> 
                         </Link>
                     </div>
                         <div className="top-bar-middle">
                             <div className='logobox_Words'>
-                                <img src="../ImagesC/Logo_Word.png" width="100%"></img>
+                                <img src="../ImagesC/FTF_Logo_Red.png" width="100%"></img>
                             </div>
                         </div>
                     <div className="top-bar-right">
