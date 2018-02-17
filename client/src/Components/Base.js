46ef3aae64522c7780197e76bb13ba653f1c2a2d
Colin Shifley <ccshifley@gmail.com>
2018-02-17 10:27:19 -0500

    DesignStuff

--- client/src/Components/Base.js
+++ client/src/Components/Base.js
@@ -5,6 +5,7 @@ import Auth from '../Modules/Auth';
 import './Base.css';
 import NavBar from './NavBar/NavBar.js';
 
+
 class Base extends Component {
 
     render() {
@@ -12,8 +13,17 @@ class Base extends Component {
             <div className='body-base'>
                 <div className='top-bar'>
                     <div className="top-bar-left">
-                        <Link to="/">Food Truck Finder</Link>
+                        <Link to="/">
+                            <div className='logobox'>
+                                <img src="../ImagesC/FTF-logo_OneColor_Truck.png" width="100%" padding="0.5rem, 0"></img>
+                            </div> 
+                        </Link>
                     </div>
+                        <div className="top-bar-middle">
+                            <div className='logobox_Words'>
+                                <img src="../ImagesC/Logo_Word.png" width="100%"></img>
+                            </div>
+                        </div>
                     <div className="top-bar-right">
                         <NavBar 
                             isAuthenticated={this.props.isAuthenticated } 
