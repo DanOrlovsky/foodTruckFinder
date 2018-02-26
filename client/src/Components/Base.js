fa896cdc84c870c0bfe129c8d4da4bbdd0cbe076
Jordan <jordan.a.rigsby@gmail.com>
2018-02-26 11:39:46 -0500

    adjustment

--- client/src/Components/Base.js
+++ client/src/Components/Base.js
@@ -20,8 +20,8 @@ class Base extends Component {
                 <div className="footer-copyright">
                     <div className="container text-center">
                     {/* Add font awesome icons */}
-                    <a href="https://www.facebook.com/FoodTruckFinderLife/" target="_Blank" class="fa fa-facebook"></a>
-                    <a href="https://twitter.com/FTFLife" target="_Blank" class="fa fa-twitter"></a>
+                    <a href="https://www.facebook.com/FoodTruckFinderLife/" target="_blank" class="fa fa-facebook"></a>
+                    <a href="https://twitter.com/FTFLife" target="_blank" class="fa fa-twitter"></a>
                     <br>
                        © <span className="arial">2018</span> Copyright Food Truck Finder Inc.
                     </div>
