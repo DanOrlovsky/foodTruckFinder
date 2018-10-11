3282cec5aab7e3100917b04b9acdb48dff680ece
Jay Wolfe <john.wolfe@ascendum.com>
2018-10-11 09:04:51 -0400

    upgraded to bcryptjs to make this project more platform universal

--- db/models/user.js
+++ db/models/user.js
@@ -1,105 +1,104 @@
-'use strict';
+"use strict";
 // ----------------------------------------------------
 // --- U S E R . J S ----------------------------------
 // ----------------------------------------------------
 // The model for the User mongo document
 
 // INCLUDES
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
-const foodTruck = require('./foodTruck');
+const bcrypt = require("bcryptjs");
+const foodTruck = require("./foodTruck");
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     // email
     email: {
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: true
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: true
     },
     // firstName
     firstName: {
-        type: String,
-        default: "",
+      type: String,
+      default: ""
     },
     // lastName
     lastName: {
-        type: String,
-        default: "",
+      type: String,
+      default: ""
     },
     // password
-    password: { type: String, required: true, },
+    password: { type: String, required: true },
     // address
     address: {
-        type: String,
-        default: "",
+      type: String,
+      default: ""
     },
     // city
     city: {
-        type: String,
-        default: "",
+      type: String,
+      default: ""
     },
     // state
     state: {
-        type: String,
-        default: "",
+      type: String,
+      default: ""
     },
     // zipcode
-    zipCode: { type: Number, required: true, },
+    zipCode: { type: Number, required: true },
     // roles User, Foodtruck, Vendor
     role: {
-        type: String,
-        enum: ['User', 'Foodtruck', 'Vendor'],
-        default: ['User']
+      type: String,
+      enum: ["User", "Foodtruck", "Vendor"],
+      default: ["User"]
     },
     // If Foodtruck role, foodtrucks go here.
-    foodTrucks: [
-        foodTruck.foodTruckSchema,
-    ],
+    foodTrucks: [foodTruck.foodTruckSchema],
     resetPasswordToken: String,
     resetPasswordExpires: Date,
     // Join date
     joined: { type: Date, default: Date.now }
-}, {
+  },
+  {
     timestamps: true
-});
+  }
+);
 
 // Hashes the password before we send it to the database.
-userSchema.pre('save', function(next) {
-    const user = this;
-    const saltFactor = 5;
-    
-    // Checks whether there's a reason to hash the password in the first place.
-    
-    if(!user.isModified('password')) return next();
+userSchema.pre("save", function(next) {
+  const user = this;
+  const saltFactor = 5;
 
-    bcrypt.genSalt(saltFactor, (err, salt) => {
-        if(err) return next(err);
-        console.log("Salting password")
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if(err) {
-                console.log(err);
-                return next(err);
-            }
-            user.password = hash;
-            next();
-        });
+  // Checks whether there's a reason to hash the password in the first place.
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(saltFactor, (err, salt) => {
+    if (err) return next(err);
+    console.log("Salting password");
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+      user.password = hash;
+      next();
     });
+  });
 });
 
 // Compares a candidate for password to a hashed password.
 userSchema.methods.comparePassword = function(candidate) {
-    return new Promise((resolve, reject) => {
-        bcrypt.compare(candidate, this.password, (err, isMatch) => {
-            console.log(err);
-            if(err) return reject(err);
-            resolve(isMatch);
-        })
-    })
-
-}
-
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidate, this.password, (err, isMatch) => {
+      console.log(err);
+      if (err) return reject(err);
+      resolve(isMatch);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
